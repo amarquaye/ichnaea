989b9f0321d1578b3d98e2a139e1e9ad1b1f6716
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-02-09 16:00:15 +0000

    Updates

--- main/number_location.py
+++ main/number_location.py
@@ -8,3 +8,9 @@ sanNumber = phonenumbers.parse(number)
 your_location = geocoder.description_for_number(sanNumber, "en")
 
 print(your_location)
+
+#get carrier information
+from phonenumbers import carrier
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
